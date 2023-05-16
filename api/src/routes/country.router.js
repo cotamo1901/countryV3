aff673fad1c818c6c07bfc8f12d9c10f9c8b5478
John Cotamo <johncotamo@hotmail.com>
2023-05-15 21:40:02 -0500

    se realiza configuracion de las rutas

--- api/src/routes/country.router.js
+++ api/src/routes/country.router.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const{getCountryApi} =require('../controllers/country.controller')
+const { getCountryApi } = require("../controllers/country.controller");
 
-router.get('/', async(req, res) => {
-
- try {
-  const country = await getCountryApi()
-  res.status(200).json(country)
- } catch (error) {
-  console.log("Aqui esta el error",error)
- }
+router.get("/", async (req, res) => {
+  try {
+    const country = await getCountryApi();
+    res.status(200).json(country);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
