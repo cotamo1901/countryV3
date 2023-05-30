f233d355409f4a8233aac2301ddc9bdbb4c7557b
john cotamo <johncotamo@hotmail.com>
2023-05-30 14:02:32 -0500

    aad store

--- client/src/views/LadinPage.jsx
+++ client/src/views/LadinPage.jsx
@@ -5,11 +5,11 @@ export default function LadingPage() {
             <div className="lad-container">
               <h1 className="title">¡PI COUNTRIES!</h1>
 
-              <img
+              {/* <img
                 src="https://w0.peakpx.com/wallpaper/860/168/HD-wallpaper-world-map-typography-names-of-countries-political-map-of-the-world.jpg"
                 alt="image"
                 className="lading-image login-button "
-              />
+              /> */}
     
               <input type="submit" value="GO!" className="prymary-button" />
               
