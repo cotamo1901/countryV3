20ee23af939a5b21875b06303e2278bd9eb2fbef
Armando Amezquita Molina <70670702+Armando-Amezquita@users.noreply.github.com>
2023-05-30 23:10:51 -0500

    add countries

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,12 @@
 import { Link } from "./Link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [cities, setCities] = useState([]);
   const getCountries = () => {
     const data = fetch("http://localhost:3001/countries")
       .then((response) => response.json())
-      .then((citys) => citys)
+      .then((citys) => setCities(citys))
       .catch((error) => console.error(error));
     console.log("data :>> ", data);
   };
@@ -13,14 +14,22 @@ export default function Home() {
     getCountries();
   }, []);
   return (
-
-
     <header>
       <div className="home">
         <div className="home-container">
-            
           <h1 className="title">Home</h1>
           <Link to="/"></Link>
+          {
+            cities.map(l=>(
+                <div>
+                    <img src={l.flagImage}/>
+                    <p>Nombre:{l.name}</p>
+                    <p>Continent:{l.continent}</p>
+
+                   
+                </div>
+            ))
+          }
         </div>
       </div>
     </header>
