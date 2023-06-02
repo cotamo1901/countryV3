ad65d42fab2cb72fa0c4662f56a9fd25086323e9
Armando Amezquita Molina <70670702+Armando-Amezquita@users.noreply.github.com>
2023-06-01 22:25:56 -0500

    add

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,36 +1,55 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
   const [cities, setCities] = useState([]);
+  const [searchCity, setSearchCity] = useState("");
+  const [originalCities, setOriginalCities] = useState([]);
+
   const getCountries = () => {
-    const data = fetch("http://localhost:3001/countries")
+    fetch("http://localhost:3001/countries")
       .then((response) => response.json())
-      .then((citys) => setCities(citys))
+      .then((cities) => {
+        setCities(cities);
+        setOriginalCities(cities);
+      })
       .catch((error) => console.error(error));
-    console.log("data :>> ", data);
   };
+
   useEffect(() => {
     getCountries();
   }, []);
-return(
+
+  function handleSearch(event) {
+    const searchTerm = event.target.value;
+    setSearchCity(searchTerm);
+
+    const filteredCities = originalCities.filter((city) =>
+      city.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCities(filteredCities);
+  }
+
+  function handleClearSearch() {
+    setSearchCity("");
+    setCities(originalCities);
+  }
+
+  console.log("cities :>> ", cities);
+
+  return (
     <div>
-        <h1>Home Countries</h1>
-        <input type="text" placeholder="Search.."  />
-
-        <div className="card-list">
-            {cities.map(country=>(
-                <div className="country-card" key={country.id}>
-                    <img src={country.flagImage} alt="Flag" />
-                    <h2>{country.name}</h2>
-                    <p>Continente:{country.continent}</p>
-
-                    
-                </div>
-            ))}
-
-        </div>
-
-
-
+      <h1>Home Countries</h1>
+      <input onChange={handleSearch} type="text" value={searchCity} placeholder="Search.." />
+      <button onClick={handleClearSearch}>Clear</button>
+      <div className="card-list">
+        {cities.map((city) => (
+          <div className="country-card" key={city.id}>
+            <img src={city.flagImage} alt="Flag" />
+            <h2>{city.name}</h2>
+            <p>Continente: {city.continent}</p>
+          </div>
+        ))}
+      </div>
     </div>
-)}
+  );
+}
