4195b7acb149febefa65be8f69b0f350fc5942b0
Armando Amezquita Molina <70670702+Armando-Amezquita@users.noreply.github.com>
2023-06-14 00:51:19 -0500

    msg

--- client/src/views/FormPage.jsx
+++ client/src/views/FormPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { connect } from "react-redux";
 import validation from "./validation";
@@ -11,18 +12,19 @@ import {
 } from "../redux/actions.js";
 
 export const Form = (props) => {
-  const [selectedContinent, setSelectedContinent] = useState("");
-  const [selectedCountries, setSelectedCountries] = useState([]);
-  const [unSelectedCountries, setUnSelectedCountries] = useState([]);
-  const [tried, setTried] = useState(0);
-  const [activity, setActivity] = useState({
-    countries: [],
+  const [selectedContinent, setSelectedContinent] = React.useState("");
+  const [selectedValue, setSelectValue] = React.useState(""); //prueba
+  const [selectedCountries, setSelectedCountries] = React.useState([]);
+  const [unSelectedCountries, setUnSelectedCountries] = React.useState([]);
+  const [tried, setTried] = React.useState(0);
+  const [activity, setActivity] = React.useState({
     name: "",
-    difficulty: "",
+    difficulty: 0,
     duration: "",
     season: "",
+    countries: [],
   });
-  const [errors, setErrors] = useState({
+  const [errors, setErrors] = React.useState({
     countries: ``,
     name: "",
     difficulty: "",
@@ -30,7 +32,7 @@ export const Form = (props) => {
     season: "",
   });
 
-  const [to, setTo] = useState([]);
+  const [to, setTo] = React.useState([]);
 
   const handleRadio = (e) => {
     setSelectedContinent(e.target.value);
@@ -38,6 +40,7 @@ export const Form = (props) => {
     props.orderCards("AA");
     props.filterSelectedCountries(to);
     setSelectedCountries([]);
+    console.log("aqui esta :>> ", e);
   };
 
   const changeTo = () => {
@@ -69,6 +72,7 @@ export const Form = (props) => {
     e.preventDefault();
     setTried(1);
     setErrors(validation(activity));
+
     if (
       !validation(activity).countries &&
       !validation(activity).difficulty &&
@@ -77,6 +81,7 @@ export const Form = (props) => {
       !validation(activity).season
     ) {
       setTried(0);
+
       await axios.post(`http://localhost:3001/activities/`, activity);
 
       setActivity({
@@ -94,6 +99,7 @@ export const Form = (props) => {
 
   const changeActivity = (e) => {
     setActivity({ ...activity, [e.target.name]: e.target.value });
+    console.log("aquiesta la actividad :>> ", e);
     if (tried === 1) {
       setErrors(validation({ ...activity, [e.target.name]: e.target.value }));
     }
@@ -109,9 +115,15 @@ export const Form = (props) => {
     setSelectedCountries(selected);
   };
 
-  useEffect(() => {
+  React.useEffect(() => {
     props.getCountries();
   }, []);
+
+  // function formPrueba(e) {
+  //   e.preventDefault();
+  //   console.log("e.target.name :>> ", e.target.name);
+  //   console.log("e.target.value :>> ", e.target.value);
+  // }
 
   return (
     <div className="container-form">
@@ -119,161 +131,177 @@ export const Form = (props) => {
       <form onSubmit={handleSubmit}>
         <fieldset className="bord-form">
           <br />
-      <fieldset>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={changeActivity} />
-        </fieldset>
-        <br />
-        <fieldset>
-        <label htmlFor="difficulty">Difficulty</label>
-        <select
-          name="difficulty"
-          onChange={changeActivity}
-          value={activity.difficulty}
-        >
-          <option value="Select">Please select</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-        </fieldset>
-        <br />
-        <fieldset>
-        <label>Season</label>
-        <select name="season" onChange={changeActivity} value={activity.season}>
-          <option value="Select">Please select</option>
-          <option value="Winter">Winter</option>
-          <option value="Spring">Spring</option>
-          <option value="Summer">Summer</option>
-          <option value="Autumm">Autumn</option>
-        </select>
-        </fieldset>
-        <br />
-        <fieldset>
-        <label>Duration</label>
-        <input
-          type="text"
-          onChange={changeActivity}
-          placeholder="duration in hours"
-          name="duration"
-          value={activity.duration}
-        />
-        </fieldset>
-        <div>
-          
-          <div width="40%"> {errors.name && <div>{errors.name}</div>}</div>
-          <div width="15%">
-            {" "}
-            {errors.difficulty && <div>{errors.difficulty}</div>}
-          </div>
-          <div width="15%"> {errors.season && <div>{errors.season}</div>}</div>
-          <div width="30%">
-            {" "}
-            {errors.duration && <div>{errors.duration}</div>}
-          </div>
-        </div>
-        <br />
-
-        <fieldset>
-          <legend>Select a continent</legend>
-
-          <div>
+          <fieldset>
+            <label htmlFor="name">Name</label>
             <input
-              type="radio"
-              id="Europe"
-              name="continent"
-              value="Europe"
-              onChange={handleRadio}
-              checked={selectedContinent === "Europe"}
+              placeholder="Activity Name"
+              type="text"
+              id="name"
+              name="name"
+              value={activity.name}
+              onChange={(e) => {
+                changeActivity(e);
+              }}
             />
-            <label htmlFor="Europe">Europe</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="Americas"
-              name="continent"
-              value="Americas"
-              onChange={handleRadio}
-              checked={selectedContinent === "Americas"}
-            />
-            <label htmlFor="Americas">Americas</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="Antarctic"
-              name="continent"
-              value="Antarctic"
-              onChange={handleRadio}
-              checked={selectedContinent === "Antarctic"}
-            />
-            <label htmlFor="Antartic">Antarctic</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="Africa"
-              name="continent"
-              value="Africa"
-              onChange={handleRadio}
-              checked={selectedContinent === "Africa"}
-            />
-            <label htmlFor="Africa">Africa</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="Asia"
-              name="continent"
-              value="Asia"
-              onChange={handleRadio}
-              checked={selectedContinent === "Asia"}
-            />
-            <label htmlFor="Asia">Asia</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="Oceania"
-              name="continent"
-              value="Oceania"
-              onChange={handleRadio}
-              checked={selectedContinent === "Oceania"}
-            />
-            <label htmlFor="Oceania">Oceania</label>
-          </div>
-        </fieldset>
-        <fieldset>
+          </fieldset>
           <br />
-        <label htmlFor="to">Select countries:</label>
-        <select
-          id="to"
-          name="to"
-          size="5"
-          multiple
-          value={unSelectedCountries}
-          onChange={handleSelectedCountries}
-        >
-          {to.length ? (
-            to.map((country) => (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            ))
-          ) : (
-            <option value="nothing">Nothing here yet!</option>
-          )}
-        </select>
-        </fieldset>
+          <fieldset>
+            <label htmlFor="difficulty">Difficulty</label>
+            <select
+              name="difficulty"
+              onChange={changeActivity}
+              value={activity.difficulty}
+            >
+              <option value="Select">Please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </fieldset>
+          <br />
+          <fieldset>
+            <label>Season</label>
+            <select
+              name="season"
+              onChange={changeActivity}
+              value={activity.season}
+            >
+              <option value="Select">Please select</option>
+              <option value="Winter">Winter</option>
+              <option value="Spring">Spring</option>
+              <option value="Summer">Summer</option>
+              <option value="Autumm">Autumn</option>
+            </select>
+          </fieldset>
+          <br />
+          <fieldset>
+            <label>Duration</label>
+            <input
+              type="text"
+              onChange={changeActivity}
+              placeholder="duration in hours"
+              name="duration"
+              value={activity.duration}
+            />
+          </fieldset>
+          <div>
+            <div width="40%"> {errors.name && <div>{errors.name}</div>}</div>
+            <div width="15%">
+              {" "}
+              {errors.difficulty && <div>{errors.difficulty}</div>}
+            </div>
+            <div width="15%">
+              {" "}
+              {errors.season && <div>{errors.season}</div>}
+            </div>
+            <div width="30%">
+              {" "}
+              {errors.duration && <div>{errors.duration}</div>}
+            </div>
+          </div>
+          <br />
 
+          <fieldset>
+            <legend>Select a continent</legend>
 
-        <input type="button" onClick={changeTo} value=">>" />
-        <input type="button" onClick={changeBack} value="<<" />
+            <div>
+              <input
+                type="radio"
+                id="Europe"
+                name="continent"
+                value="Europe"
+                onChange={handleRadio}
+                checked={selectedContinent === "Europe"}
+              />
+              <label htmlFor="Europe">Europe</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="Americas"
+                name="continent"
+                value="Americas"
+                onChange={handleRadio}
+                checked={selectedContinent === "Americas"}
+              />
+              <label htmlFor="Americas">Americas</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="Antarctic"
+                name="continent"
+                value="Antarctic"
+                onChange={handleRadio}
+                checked={selectedContinent === "Antarctic"}
+              />
+              <label htmlFor="Antartic">Antarctic</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="Africa"
+                name="continent"
+                value="Africa"
+                onChange={handleRadio}
+                checked={selectedContinent === "Africa"}
+              />
+              <label htmlFor="Africa">Africa</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="Asia"
+                name="continent"
+                value="Asia"
+                onChange={handleRadio}
+                checked={selectedContinent === "Asia"}
+              />
+              <label htmlFor="Asia">Asia</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="Oceania"
+                name="continent"
+                value="Oceania"
+                onChange={handleRadio}
+                checked={selectedContinent === "Oceania"}
+              />
+              <label htmlFor="Oceania">Oceania</label>
+            </div>
+          </fieldset>
+          <fieldset>
+            <br />
+            <label htmlFor="to">Select countries:</label>
+            <select
+              id="to"
+              name="to"
+              size="5"
+              multiple
+              value={unSelectedCountries}
+              onChange={handleSelectedCountries}
+            >
+              {to.length ? (
+                to.map((country, index) => (
+                  <option key={index} value={country}>
+                    {country}
+                  </option>
+                ))
+              ) : (
+                <option value="nothing">Nothing here left!</option>
+              )}
+            </select>
+          </fieldset>
 
-        <input type="submit" value="Save Activity" />
+          <input type="button" onClick={changeTo} value=">>" />
+          <input type="button" onClick={changeBack} value="<<" />
+
+          <input type="submit" value="Save Activity" />
+
+          <Link to={`/home/`}>Home</Link>
         </fieldset>
       </form>
     </div>
@@ -304,4 +332,3 @@ export function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Form);
-
